1846cebd95e1211ce267160d86d53455e7a8b7ac
HuangSir66 <129098871+HuangSir66@users.noreply.github.com>
2023-12-04 20:00:35 +0800

    新增了蚕只状态表

---
+++ src/main/java/com/example/culture_service/domain/UserSilkwormStatu.java
@@ -0,0 +1,19 @@
+package com.example.culture_service.domain;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * @author HUANGZHEMING
+ * @date 2023/12/4
+ * @Description
+ */
+@Data
+
+public class UserSilkwormStatu {
+
+    private Long id;
+
+    private String statu;
+
+}
---
+++ src/main/java/com/example/culture_service/mapper/UserSilkwormStatuMapper.java
@@ -0,0 +1,12 @@
+package com.example.culture_service.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.culture_service.domain.UserSilkwormStatu;
+
+/**
+ * @author HUANGZHEMING
+ * @date 2023/12/4
+ * @Description
+ */
+public interface UserSilkwormStatuMapper extends BaseMapper<UserSilkwormStatu> {
+}
