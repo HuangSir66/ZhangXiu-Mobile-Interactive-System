18d8e2c09afcb80b52e09e059cb340d1229bf012
HuangSir66 <129098871+HuangSir66@users.noreply.github.com>
2023-12-06 15:51:16 +0800

    新增了蚕长大后userasset的变化

--- src/main/java/com/example/culture_service/controller/UserSilkWormNumberController.java
+++ src/main/java/com/example/culture_service/controller/UserSilkWormNumberController.java
@@ -42,7 +42,9 @@ public class UserSilkWormNumberController {
         }
     }
 
-
+    /*
+    熟蚕长大
+     */
 
     @GetMapping("/silkworm_number/ripe_silkworm_grow/{userid}")
     public JsonResult<List<UserSilkwormNumber>> addRipeSilkworm(@PathVariable Long userid) {
@@ -52,13 +54,13 @@ public class UserSilkWormNumberController {
 
             // 2. 修改蚕的状态数量
             for (UserSilkwormNumber silkworm : silkworms) {
-                if ("熟蚕".equals(silkworm.getSilkwormStatu())) {
+                if ("未收获蚕茧".equals(silkworm.getSilkwormStatu())) {
                     // 熟蚕数量加一
                     silkworm.setSilkwormNumber(silkworm.getSilkwormNumber() + 1);
                     UpdateWrapper<UserSilkwormNumber> updateWrapper = new UpdateWrapper<>();
                     updateWrapper.eq("id", silkworm.getId());
                     userSilkwormNumberService.update(silkworm, updateWrapper);
-                } else if ("蚁蚕".equals(silkworm.getSilkwormStatu())) {
+                } else if ("熟蚕".equals(silkworm.getSilkwormStatu())) {
                     // 蚁蚕数量减一
                     silkworm.setSilkwormNumber(silkworm.getSilkwormNumber() - 1);
                     // 在更新时使用UpdateWrapper指定更新条件
@@ -76,7 +78,9 @@ public class UserSilkWormNumberController {
         }
     }
 
-
+    /*
+        蚁蚕长大
+     */
     @GetMapping("/silkworm_number/ant_silkworm_grow/{userid}")
     public JsonResult<List<UserSilkwormNumber>> addAntSilkworm(@PathVariable Long userid) {
         try {
@@ -87,13 +91,13 @@ public class UserSilkWormNumberController {
             for (UserSilkwormNumber silkworm : silkworms) {
                 if ("熟蚕".equals(silkworm.getSilkwormStatu())) {
                     // 熟蚕数量加一
-                    silkworm.setSilkwormNumber(silkworm.getSilkwormNumber() - 1);
+                    silkworm.setSilkwormNumber(silkworm.getSilkwormNumber() + 1);
                     UpdateWrapper<UserSilkwormNumber> updateWrapper = new UpdateWrapper<>();
                     updateWrapper.eq("id", silkworm.getId());
                     userSilkwormNumberService.update(silkworm, updateWrapper);
                 } else if ("蚁蚕".equals(silkworm.getSilkwormStatu())) {
                     // 蚁蚕数量减一
-                    silkworm.setSilkwormNumber(silkworm.getSilkwormNumber() + 1);
+                    silkworm.setSilkwormNumber(silkworm.getSilkwormNumber() - 1);
                     // 在更新时使用UpdateWrapper指定更新条件
                     UpdateWrapper<UserSilkwormNumber> updateWrapper = new UpdateWrapper<>();
                     updateWrapper.eq("id", silkworm.getId());
@@ -108,4 +112,46 @@ public class UserSilkWormNumberController {
             return new JsonResult<>("400","修改失败");
         }
     }
+
+    /*
+    收货蚕茧
+     */
+    @GetMapping("/silkworm_number/get_silkworm_cocoon/{userid}")
+    public JsonResult<List<UserSilkwormNumber>> getSilkwormCocoon(@PathVariable Long userid) {
+        try {
+            // 1. 查询指定用户的蚕信息
+            Integer Silkworm_cocoon = null;
+            List<UserSilkwormNumber> silkworms = userSilkwormNumberService.getSilkwormNumberByUserId(userid);
+            // 2. 修改蚕的状态数量
+            for (UserSilkwormNumber silkworm : silkworms) {
+                if ("未收获蚕茧".equals(silkworm.getSilkwormStatu())) {
+                    // 未收货蚕茧清零
+                    Silkworm_cocoon = silkworm.getSilkwormNumber();
+                    silkworm.setSilkwormNumber(0);
+                    UpdateWrapper<UserSilkwormNumber> updateWrapper = new UpdateWrapper<>();
+                    updateWrapper.eq("id", silkworm.getId());
+                    userSilkwormNumberService.update(silkworm, updateWrapper);
+                }
+            }
+            for(UserSilkwormNumber silkworm:silkworms){
+                if ("蚕蛹".equals(silkworm.getSilkwormStatu())) {
+                    // 更新蚕蛹数量
+                    if(Silkworm_cocoon!=null){
+                        silkworm.setSilkwormNumber(silkworm.getSilkwormNumber()+Silkworm_cocoon);
+                        UpdateWrapper<UserSilkwormNumber> updateWrapper = new UpdateWrapper<>();
+                        updateWrapper.eq("id", silkworm.getId());
+                        userSilkwormNumberService.update(silkworm, updateWrapper);
+                    }
+                }
+            }
+
+            // 4. 返回修改后的结果
+            return new JsonResult<>("200","修改成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new JsonResult<>("400","修改失败");
+        }
+    }
+
+
 }
